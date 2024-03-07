1ffaa07006a987df9ba516bb354bf9256528f222
BinyamMamo <binyammamo01@gmail.com>
2024-03-07 13:15:37 +0400

    fix formatting issues with 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,22 +3,24 @@
 Generates a Pascal's Triangle with `n` rows.
 """
 
+
 def pascal_triangle(n):
-  """
-  Returns the pascal triangle of size n
-  """
-  triangle = []
+    """
+    Returns the pascal triangle of size n
+    """
+    triangle = []
 
-  if (n < 2): return [1] if n == 1 else []
+    if (n < 2):
+        return [1] if n == 1 else []
 
-  for y in range(n + 1):
-    row = []
-    for x in range(y + 1):
-      if y == x or x == 0:
-        row.append(1)
-      else:
-        row.append(triangle[y - 1][x] + triangle[y - 1][x - 1])
-    if row:
-      triangle.append(row)
+    for y in range(n + 1):
+        row = []
+        for x in range(y + 1):
+            if y == x or x == 0:
+                row.append(1)
+            else:
+                row.append(triangle[y - 1][x] + triangle[y - 1][x - 1])
+        if row:
+            triangle.append(row)
 
-  return triangle
+    return triangle
