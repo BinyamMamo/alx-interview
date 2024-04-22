5e53d892f0be35b7ee64c4c3246253428fd736f4
BinyamMamo <binyammamo01@gmail.com>
2024-04-23 01:25:37 +0400

    feat: implement in-place rotation algorithm for 2D matrices

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+0. Rotate 2D Matrix
+mandatory
+Given an n x n 2D matrix, rotate it 90 degrees clockwise.
+"""
+from typing import List
+
+
+def rotate_2d_matrix(matrix: List[List[int]]) -> None:
+    """
+    Rotates a 2D matrix 90 degrees clockwise in place
+    """
+    left, right = 0, len(matrix) - 1
+
+    while left < right:
+        for i in range(right - left):
+            top, bottom = left, right
+            topLeft = matrix[top][left + i]
+
+            matrix[top][left + i] = matrix[bottom - i][left]
+            matrix[bottom - i][left] = matrix[bottom][right - i]
+            matrix[bottom][right - i] = matrix[top + i][right]
+            matrix[top + i][right] = topLeft
+        left += 1
+        right -= 1
